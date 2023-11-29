074ca5db97360b3debae20b83e3bfc682c52ce4f
ZEY <Zineb.Chafiki@emsi-edu.ma>
2023-11-29 22:53:19 +0100

    First Commit

---
+++ customer-serviice/src/main/java/orgbillsapp/customerserviice/CustomerServiiceApplication.java
@@ -0,0 +1,13 @@
+package orgbillsapp.customerserviice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerServiiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomerServiiceApplication.class, args);
+	}
+
+}
---
+++ customer-serviice/src/test/java/orgbillsapp/customerserviice/CustomerServiiceApplicationTests.java
@@ -0,0 +1,13 @@
+package orgbillsapp.customerserviice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerServiiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
